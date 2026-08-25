22647b8daa9423b7b0edd66903f8a07403e5a0ad
Aayush <yaayush303@gmail.com>
2026-08-25 19:51:32 +0000

    csci

---
+++ firstprog.cpp
@@ -0,0 +1,10 @@
+//Aayush Yadav
+#include <iostream>          // needed to perform C++ I/O
+using namespace std;
+
+int main() {
+    cout << "Welcome to my first C++ program." << endl;
+    cout << "I am learning how to display output." << endl;
+
+    return 0;
+}
